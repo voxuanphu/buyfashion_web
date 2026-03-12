7416e2255198e9508294e531675521d069909c45
voxuanphu <126144899+voxuanphu@users.noreply.github.com>
2026-03-13 00:34:49 +0700

    add shopee connector

---
+++ connectors/shopee_connector.py
@@ -0,0 +1,13 @@
+def fetch_products():
+
+    return [
+        {
+        "title":"Áo thun nam cotton",
+        "price":189000,
+        "image":"https://via.placeholder.com/400",
+        "shop":"Fashion Store",
+        "rating":4.8,
+        "affiliate_url":"https://s.shopee.vn/8V491WRRLx",
+        "review":"Áo cotton form rộng dễ mặc."
+        }
+    ]
