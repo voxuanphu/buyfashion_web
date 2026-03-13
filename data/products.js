ed30f333486eb04cd0cc257b3ad5c282a22ed985
voxuanphu <126144899+voxuanphu@users.noreply.github.com>
2026-03-13 19:45:57 +0700

    add products 6-10

--- data/products.js
+++ data/products.js
@@ -74,5 +74,78 @@
     link: "https://s.shopee.vn/1BHZtCJzJX",
     image: "assets/products/ao-thun-5.jpg"
   }
+{
+  id: 6,
+  name: "Áo thun nam basic form fit",
+  category: "Áo thun",
+  price: 119000,
+  oldPrice: 159000,
+  rating: 4.7,
+  sold: "1.5k",
+  discount: 25,
+  shop: "Shopee",
+  badge: "Bán chạy",
+  link: "https://shopee.vn/",
+  image: "assets/products/ao-thun-6.jpg"
+},
 
+{
+  id: 7,
+  name: "Áo polo nam thể thao co giãn",
+  category: "Áo polo",
+  price: 189000,
+  oldPrice: 249000,
+  rating: 4.8,
+  sold: "2k",
+  discount: 24,
+  shop: "Lazada",
+  badge: "Phổ biến",
+  link: "https://lazada.vn/",
+  image: "assets/products/ao-polo-1.jpg"
+},
+
+{
+  id: 8,
+  name: "Áo sơ mi nam công sở slim fit",
+  category: "Áo sơ mi",
+  price: 259000,
+  oldPrice: 329000,
+  rating: 4.9,
+  sold: "980",
+  discount: 21,
+  shop: "Tiki",
+  badge: "Công sở",
+  link: "https://tiki.vn/",
+  image: "assets/products/ao-somi-1.jpg"
+},
+
+{
+  id: 9,
+  name: "Áo thun nam oversize streetwear",
+  category: "Áo thun",
+  price: 169000,
+  oldPrice: 219000,
+  rating: 4.6,
+  sold: "1.2k",
+  discount: 23,
+  shop: "Shopee",
+  badge: "Hot trend",
+  link: "https://shopee.vn/",
+  image: "assets/products/ao-thun-7.jpg"
+},
+
+{
+  id: 10,
+  name: "Áo polo nam basic cao cấp",
+  category: "Áo polo",
+  price: 199000,
+  oldPrice: 259000,
+  rating: 4.8,
+  sold: "1k",
+  discount: 23,
+  shop: "Lazada",
+  badge: "Đáng mua",
+  link: "https://lazada.vn/",
+  image: "assets/products/ao-polo-2.jpg"
+},
 ];
